6c152f17eee1779557e82a7ff34e48617d7ca5dc
Your Name <swchen@mit.edu>
2015-09-25 22:49:49 -0400

    corrected Reverser.java

--- Reverser.java
+++ Reverser.java
@@ -1,69 +1,86 @@
 public class Reverser {
 
-	private char [] words;
+  private char [] words;
 
-	public Reverser(char [] wordArray){
-		words = wordArray;
-	}
+  public Reverser(char [] wordArray){
+    words = wordArray;
+  }
 
-	public static void main(String [] args){
-		char [] wordArray = {'a', ' ', 'b', 'c', ' ', ' ', 'd'};
-		Reverser r = new Reverser(wordArray);
-		r.reverseWordsInPlace();
-		System.out.println(r.printToString());
+  public static void main(String [] args){
+    char [] wordArray = {'a', ' ', 'b', 'c', ' ', ' ', 'd'};
+    Reverser r = new Reverser(wordArray);
+    r.reverseWordsInPlace();
+    System.out.println(r.printToString());
 
-	}
+  }
 
-	/*
-	* reverses the words (i.e. groups of letters separated by spaces) in a char array in place 
-	* 
-	*/
-	private void reverseWordsInPlace (){
-		//reverses all the letters in the array
-		for (int i = 0; i < words.length/2; i++){
-			char tmp = words[i];
-			words[i] = words[words.length - 1 - i];
-			words[words.length - 1 - i] = tmp;
-		}
-		//find the spaces in the reversed array
+  /*
+  * reverses the words (i.e. groups of letters separated by spaces) in a char array in place 
+  * 
+  */
+  private void reverseWordsInPlace (){
+    //reverses all the letters in the array
+    reverseWord(0, words.length-1);
 
-		boolean inWord = false;
-		for (int i = 0; i < words.length; i++){
-			int startIndex = 0, endIndex = 0;
-			if (!inWord && words[i] != ' '){
-				startIndex = i;
-				inWord = true;
-			}
-			else if (inWord && words[i] == ' '){
-				endIndex = i-1;
-				inWord = false;
-				reverseWord(startIndex, endIndex);
-			}
+    System.out.println(new String(words));
+    //find the spaces in the reversed array
 
-			else if (inWord && i == words.length-1){
-				endIndex = i;
-				reverseWord(startIndex, endIndex);
-			}
-		}
-	}
+    boolean inWord = false;
+    int i = 0;
+    while(i < words.length) {
+      while(words[i] == ' '){
+        i++;
+      }
+
+      int j = i;
+      while(j < words.length && words[j] != ' ') {
+        j++;
+      }
+      j--;
+      System.out.println(i + ":" + j);
+      reverseWord(i, j);
+      i = j+1;
+    }
+    
+    
+    /*for (int i = 0; i < words.length; i++){
+      int startIndex = 0, endIndex = 0;
+      if (!inWord && words[i] != ' '){
+        startIndex = i;
+        inWord = true;
+      }
+      else if (inWord && words[i] == ' '){
+        endIndex = i-1;
+        inWord = false;
+        reverseWord(startIndex, endIndex);
+        //System.out.println(new String(words));
+      }
+
+      else if (inWord && i == words.length-1){
+        endIndex = i;
+        reverseWord(startIndex, endIndex);
+        //System.out.println(new String(words));
+      }
+    }*/
+  }
 
 
-	/*
-	* reverses a single word in place
-	* 
-	* @param startIndex start index of the word
-	* @param endIndex end index of the word
-	*/
-	private void reverseWord (int startIndex, int endIndex){
-		for (int i = startIndex; i < (startIndex + endIndex) / 2; i++){
-			char tmp = words[i];
-			words[i] = words[endIndex - (i - startIndex)];
-			words[endIndex - (i - startIndex)] = tmp;
-		}
-	}
+  /*
+  * reverses a single word in place
+  * 
+  * @param startIndex start index of the word
+  * @param endIndex end index of the word
+  */
+  private void reverseWord (int startIndex, int endIndex){
+    for (int i = startIndex; i <= (startIndex + endIndex) / 2; i++){
+      char tmp = words[i];
+      words[i] = words[endIndex - (i - startIndex)];
+      words[endIndex - (i - startIndex)] = tmp;
+    }
+  }
 
-	public String printToString(){
-		return new String(words);
-	}
+  public String printToString(){
+    return new String(words);
+  }
 
 }
